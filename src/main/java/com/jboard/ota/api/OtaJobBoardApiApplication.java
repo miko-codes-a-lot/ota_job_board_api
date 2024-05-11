d590ba57487d34fa905ea5e9d9d16c989a2c9607
Miko Chu <miko.codecrafters@gmail.com>
2024-05-11 12:25:35 +0800

    project init

---
+++ src/main/java/com/jboard/ota/api/OtaJobBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.jboard.ota.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtaJobBoardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtaJobBoardApiApplication.class, args);
+    }
+
+}
