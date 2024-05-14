3af270d1fb51243d0eb29f291c04cb15b6ce4de9
Miko Chu <miko.codecrafters@gmail.com>
2024-05-14 20:16:10 +0800

    modify UserEntity to remove lombok as it's causing bugs in jpa

--- src/main/java/com/jboard/ota/api/mappers/UserMapper.java
+++ src/main/java/com/jboard/ota/api/mappers/UserMapper.java
@@ -2,6 +2,7 @@ package com.jboard.ota.api.mappers;
 
 import com.jboard.ota.api.models.dto.UserDTO;
 import com.jboard.ota.api.models.entities.UserEntity;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,14 +22,14 @@ public class UserMapper implements Mapper<UserEntity, UserDTO> {
 
     @Override
     public UserEntity toEntity(UserDTO dto) {
-        return UserEntity.builder()
-                .id(dto.getId())
-                .username(dto.getUsername())
-                .firstName(dto.getFirstName())
-                .lastName(dto.getLastName())
-                .email(dto.getEmail())
-                .postedJobBefore(dto.isPostedJobBefore())
-                .realmId(dto.getRealmId())
-                .build();
+        val user = new UserEntity();
+        user.setId(dto.getId());
+        user.setUsername(dto.getUsername());
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setEmail(dto.getEmail());
+        user.setPostedJobBefore(dto.isPostedJobBefore());
+        user.setRealmId(dto.getRealmId());
+        return user;
     }
 }
--- src/main/java/com/jboard/ota/api/models/entities/UserEntity.java
+++ src/main/java/com/jboard/ota/api/models/entities/UserEntity.java
@@ -1,20 +1,11 @@
 package com.jboard.ota.api.models.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
-
 @Entity
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserEntity {
     @Id
     private String id;
@@ -34,4 +25,94 @@ public class UserEntity {
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<KeycloakRole> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "createdBy")
+    private Set<Job> createdJobs;
+    @OneToMany(mappedBy = "lastModifiedBy")
+    private Set<Job> modifiedJobs;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRealmId() {
+        return realmId;
+    }
+
+    public void setRealmId(String realmId) {
+        this.realmId = realmId;
+    }
+
+    public boolean isPostedJobBefore() {
+        return postedJobBefore;
+    }
+
+    public void setPostedJobBefore(boolean postedJobBefore) {
+        this.postedJobBefore = postedJobBefore;
+    }
+
+    public Set<KeycloakRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<KeycloakRole> roles) {
+        this.roles = roles;
+    }
+
+    public Set<Job> getCreatedJobs() {
+        return createdJobs;
+    }
+
+    public void setCreatedJobs(Set<Job> createdJobs) {
+        this.createdJobs = createdJobs;
+    }
+
+    public Set<Job> getModifiedJobs() {
+        return modifiedJobs;
+    }
+
+    public void setModifiedJobs(Set<Job> modifiedJobs) {
+        this.modifiedJobs = modifiedJobs;
+    }
+
+    @Override
+    public String toString() {
+        return STR."UserEntity{id='\{id}',username='\{username}', firstName='\{firstName}', lastName='\{lastName}', email='\{email}', realmId='\{realmId}'}";
+    }
 }
