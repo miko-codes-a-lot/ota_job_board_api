5e27c264efe34bcc7824c4de5a844c07a733b1fb
Miko Chu <miko.codecrafters@gmail.com>
2024-05-14 09:18:11 +0800

    add keycloakRole entity and role repository for data access

---
+++ src/main/java/com/jboard/ota/api/models/entities/KeycloakRole.java
@@ -0,0 +1,26 @@
+package com.jboard.ota.api.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class KeycloakRole {
+    @Id
+    private String id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<UserEntity> users;
+}
