ac9cb4c947a196d7403be3dfa1ee999ef9a4284f
Miko Chu <miko.codecrafters@gmail.com>
2024-05-14 20:14:11 +0800

    add OccupationCategoryRepository for data access

---
+++ src/main/java/com/jboard/ota/api/repositories/OccupationCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.jboard.ota.api.repositories;
+
+import com.jboard.ota.api.models.entities.OccupationCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface OccupationCategoryRepository extends JpaRepository<OccupationCategory, UUID> {
+}
