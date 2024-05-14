ea50bfc46a050bf188ae213715e2bd741f59f37e
Miko Chu <miko.codecrafters@gmail.com>
2024-05-14 20:14:25 +0800

    add JobDescription entity

---
+++ src/main/java/com/jboard/ota/api/models/entities/JobDescription.java
@@ -0,0 +1,76 @@
+package com.jboard.ota.api.models.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+public class JobDescription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private String value;
+
+    @ManyToMany
+    private Set<Job> jobs;
+
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @UpdateTimestamp
+    private Date dateUpdated;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Set<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(Set<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+}
