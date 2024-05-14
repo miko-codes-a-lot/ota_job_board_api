5c24ae1d3f6f922683c6b621525c2975b70f8a8c
Miko Chu <miko.codecrafters@gmail.com>
2024-05-14 20:17:38 +0800

    modify UserController to get all users by realmId & add authentication for debugging purposes

--- src/main/java/com/jboard/ota/api/controllers/UserController.java
+++ src/main/java/com/jboard/ota/api/controllers/UserController.java
@@ -1,16 +1,37 @@
 package com.jboard.ota.api.controllers;
 
+import com.jboard.ota.api.AppConfig;
+import com.jboard.ota.api.models.dto.UserDTO;
+import com.jboard.ota.api.repositories.OccupationCategoryRepository;
+import com.jboard.ota.api.repositories.OccupationRepository;
+import com.jboard.ota.api.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+    private final AppConfig appConfig;
+
+    public UserController(UserService userService, AppConfig appConfig, OccupationRepository occupationRepository, OccupationCategoryRepository occupationCategoryRepository) {
+        this.userService = userService;
+        this.appConfig = appConfig;
+    }
+
     @GetMapping("/")
-    public List<String> getAll() {
-        return List.of("Miko Chu", "Darwin Chu", "Arvin Chu", "Aron Garcia");
+    public List<UserDTO> findAll() {
+        return userService.findAllByRealmId(appConfig.realmId());
+    }
+
+    @GetMapping("/my-session")
+    public Authentication authentication(Authentication authentication) {
+        return authentication;
     }
 }
