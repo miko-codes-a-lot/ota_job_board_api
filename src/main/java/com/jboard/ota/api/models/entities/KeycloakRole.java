71b4a22d7e159808148939ce956fddb8834f2c20
Miko Chu <miko.codecrafters@gmail.com>
2024-05-14 20:18:05 +0800

    modify KeycloakRole to remove lombok as it's causing jpa bugs

--- src/main/java/com/jboard/ota/api/models/entities/KeycloakRole.java
+++ src/main/java/com/jboard/ota/api/models/entities/KeycloakRole.java
@@ -3,18 +3,10 @@ package com.jboard.ota.api.models.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Entity
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class KeycloakRole {
     @Id
     private String id;
@@ -23,4 +15,28 @@ public class KeycloakRole {
 
     @ManyToMany(mappedBy = "roles")
     private Set<UserEntity> users;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserEntity> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<UserEntity> users) {
+        this.users = users;
+    }
 }
