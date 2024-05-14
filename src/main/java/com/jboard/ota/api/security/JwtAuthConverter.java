3a4423f5cb1b1a30ada78d73cdb658ae18dbf619
Miko Chu <miko.codecrafters@gmail.com>
2024-05-14 20:18:25 +0800

    modify JwtAuthConverter to remove unused imports

--- src/main/java/com/jboard/ota/api/security/JwtAuthConverter.java
+++ src/main/java/com/jboard/ota/api/security/JwtAuthConverter.java
@@ -1,6 +1,7 @@
 package com.jboard.ota.api.security;
 
 import jakarta.annotation.Nonnull;
+import lombok.val;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
